76d6771bd94f8a513bca1996ab007869c9119e01
P4rth <parth_sinha@yahoo.com>
2024-09-28 21:06:51 +0100

    added text component

--- src/components/Text/index.stories.tsx
+++ src/components/Text/index.stories.tsx
@@ -16,6 +16,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
+        as: "h1",
         children: "Testing storybook with react and learning things"
     },
 };
